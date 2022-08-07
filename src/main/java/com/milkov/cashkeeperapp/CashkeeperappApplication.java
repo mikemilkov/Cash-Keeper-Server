abb4c0135a60b22ef0a68e5527c852ab6d2776d5
Mikhail Milkov <m.milkov@haulmont.com>
2022-08-07 10:04:02 +0300

    Initial commit

---
+++ src/main/java/com/milkov/cashkeeperapp/CashkeeperappApplication.java
@@ -0,0 +1,13 @@
+package com.milkov.cashkeeperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashkeeperappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashkeeperappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milkov/cashkeeperapp/CashkeeperappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milkov.cashkeeperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashkeeperappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
